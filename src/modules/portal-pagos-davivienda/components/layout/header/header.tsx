1df887ac8a232376292e161e9c6c0e367e761071
x <x@x.x>
2026-09-09 16:55:12 -0500

    fix: adjust layout and styling in header component for better alignment and accessibility

--- src/modules/portal-pagos-davivienda/components/layout/header/header.tsx
+++ src/modules/portal-pagos-davivienda/components/layout/header/header.tsx
@@ -12,7 +12,7 @@ export function Header() {
 
   return (
     <header className="bg-brand text-white">
-      <Container className="flex h-[70px] flex-wrap items-center min-[576px]:h-[117px] min-[576px]:content-center min-[768px]:h-[90px]">
+      <Container className="flex h-[70px] flex-wrap items-center min-[576px]:h-[117px] min-[576px]:content-around min-[768px]:h-[90px]">
         <div className="flex h-full min-w-0 flex-1 items-center min-[576px]:mt-[10px] min-[576px]:h-auto min-[576px]:w-full min-[576px]:flex-none min-[576px]:justify-center min-[576px]:pt-[10px] min-[768px]:mt-0 min-[768px]:w-[240px] min-[768px]:justify-start min-[768px]:pt-0">
           <a href="#/" aria-label="logo" className="block w-full max-w-[270px] min-[768px]:max-w-none">
             <Image
@@ -38,14 +38,14 @@ export function Header() {
         </button>
 
         <nav className="hidden w-full min-[576px]:block min-[768px]:w-auto min-[768px]:flex-1">
-          <ul className="flex items-center justify-center gap-8 min-[576px]:leading-[46px] min-[768px]:justify-end">
+          <ul className="flex items-end justify-center gap-8 min-[576px]:leading-[46px] min-[768px]:justify-end">
             {headerNav.map(({ label, href, target, rel }) => (
               <li key={label}>
                 <a
                   href={href}
                   target={target}
                   rel={rel}
-                  className="text-[14px] leading-[21px] text-white transition-colors duration-300 hover:text-[#ff9393]"
+                  className="text-[14px] leading-[21px] text-white transition-colors duration-300 hover:text-[#ff9393] underline"
                 >
                   {label}
                 </a>
